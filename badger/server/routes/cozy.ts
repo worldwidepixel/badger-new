af24b9e66cfbb32ad0724631510b2f484e655a74
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-05-11 23:46:18 -0700

    Test

--- badger/server/routes/cozy.ts
+++ badger/server/routes/cozy.ts
@@ -12,7 +12,7 @@ export default defineEventHandler(async (event) => {
     textToSVG = TextToSVG.loadSync("public/fonts/static/Inter-Medium.ttf");
 
   } else {
-    textToSVG = TextToSVG.loadSync("../../../public/fonts/static/Inter-Medium.ttf");
+    textToSVG = TextToSVG.loadSync("public/fonts/static/Inter-Medium.ttf");
   }
 
   var textToSVGBold: TextToSVG;
@@ -25,7 +25,7 @@ export default defineEventHandler(async (event) => {
 
   } else {
     textToSVGBold = TextToSVG.loadSync(
-      "../../../public/fonts/static/Inter-ExtraBold.ttf",
+      "public/fonts/static/Inter-ExtraBold.ttf",
     );
   }
 
