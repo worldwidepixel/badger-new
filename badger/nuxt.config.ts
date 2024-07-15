1bf5726229e0e5b32169079d0c2886314ca5ad55
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-07-14 22:59:56 -0700

    The Great Divorce: Part Two (The Bugfixes, Part I)

--- badger/nuxt.config.ts
+++ badger/nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  ssr: false,
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-lucide-icons', '@nuxtjs/color-mode'],
-  colorMode: {
-    classSuffix: ''
-  },
-  runtimeConfig: {
-    baseUrl: process.env.NODE_ENV === 'production' ? 'https://badger-staging.worldwidepixel.ca' : 'http://localhost:3000'
-  }
-})
+	devtools: { enabled: true },
+	ssr: false,
+	modules: ["@nuxtjs/tailwindcss", "nuxt-lucide-icons", "@nuxtjs/color-mode"],
+	colorMode: {
+		classSuffix: "",
+	},
+	runtimeConfig: {
+		public: {
+			apiBase:
+				process.env.NODE_ENV === "production"
+					? "https://badger-api-staging.worldwidepixel.ca"
+					: "http://localhost:3000",
+		},
+	},
+});
--- badger/pages/index.vue
+++ badger/pages/index.vue
@@ -1,68 +1,104 @@
 <script setup lang="ts">
-const { $resetBus } = useNuxtApp()
-const runtimeConfig = useRuntimeConfig()
+const { $resetBus } = useNuxtApp();
+const runtimeConfig = useRuntimeConfig();
 const route = useRoute();
 
+console.log(runtimeConfig.public.apiBase);
+
 async function urlToData(url: string) {
-	let blob = await fetch(url).then(r => r.blob());
-	let dataUrl = await new Promise(resolve => {
+	let blob = await fetch(url).then((r) => r.blob());
+	let dataUrl = await new Promise((resolve) => {
 		let reader = new FileReader();
 		reader.onload = () => resolve(reader.result);
 		reader.readAsDataURL(blob);
 	});
-	return dataUrl
-};
+	return dataUrl;
+}
 
 // INITAL DECLARATIONS
 
-const topText = ref('Made for')
-const topTextColor = ref('#FFFFFF')
-const topBackgroundColor = ref('#8F004C')
-const bottomText = ref('You')
-const bottomTextColor = ref('#FF0066')
-const bottomBackgroundColor = ref('#61003D')
-const badgeIconUrl = ref('https://badger-staging.worldwidepixel.ca/badger.png')
+const topText = ref("Made for");
+const topTextColor = ref("#FFFFFF");
+const topBackgroundColor = ref("#8F004C");
+const bottomText = ref("You");
+const bottomTextColor = ref("#FF0066");
+const bottomBackgroundColor = ref("#61003D");
+const badgeIconUrl = ref("https://badger-staging.worldwidepixel.ca/badger.png");
 
 // LOAD FROM SAVED
 
 onMounted(() => {
-	topText.value = localStorage.getItem('topText') || "Made for";
-	topTextColor.value = localStorage.getItem('topTextColor') || '#FFFFFF';
-	topBackgroundColor.value = localStorage.getItem('topBackgroundColor') || '#8F004C';
-	bottomText.value = localStorage.getItem('bottomText') || 'You';
-	bottomTextColor.value = localStorage.getItem('bottomTextColor') || '#FF0066';
-	bottomBackgroundColor.value = localStorage.getItem('bottomBackgroundColor') || '#61003D';
-	badgeIconUrl.value = localStorage.getItem('badgeIconUrl') || 'https://badger-staging.worldwidepixel.ca/badger.png';
-})
+	topText.value = localStorage.getItem("topText") || "Made for";
+	topTextColor.value = localStorage.getItem("topTextColor") || "#FFFFFF";
+	topBackgroundColor.value =
+		localStorage.getItem("topBackgroundColor") || "#8F004C";
+	bottomText.value = localStorage.getItem("bottomText") || "You";
+	bottomTextColor.value =
+		localStorage.getItem("bottomTextColor") || "#FF0066";
+	bottomBackgroundColor.value =
+		localStorage.getItem("bottomBackgroundColor") || "#61003D";
+	badgeIconUrl.value =
+		localStorage.getItem("badgeIconUrl") ||
+		"https://badger-staging.worldwidepixel.ca/badger.png";
+});
 
 // LOAD FROM URL
 
 onMounted(() => {
-	topBackgroundColor.value = route.query.gradientStart?.toString() || '#8F004C';
-	bottomBackgroundColor.value = route.query.gradientEnd?.toString() || '#61003D';
-	topText.value = route.query.lineOne?.toString() || 'Made for';
-	bottomText.value = route.query.lineTwo?.toString() || 'You';
-	topTextColor.value = (route.query.colorOne?.toString() || route.query.colourOne?.toString()) || '#FFFFFF';
-	bottomTextColor.value = (route.query.colorTwo?.toString() || route.query.colourTwo?.toString()) || '#FF0066';
-	badgeIconUrl.value = route.query.iconUrl?.toString() || 'https://badger-staging.worldwidepixel.ca/badger.png';
-})
+	topBackgroundColor.value =
+		route.query.gradientStart?.toString() || "#8F004C";
+	bottomBackgroundColor.value =
+		route.query.gradientEnd?.toString() || "#61003D";
+	topText.value = route.query.lineOne?.toString() || "Made for";
+	bottomText.value = route.query.lineTwo?.toString() || "You";
+	topTextColor.value =
+		route.query.colorOne?.toString() ||
+		route.query.colourOne?.toString() ||
+		"#FFFFFF";
+	bottomTextColor.value =
+		route.query.colorTwo?.toString() ||
+		route.query.colourTwo?.toString() ||
+		"#FF0066";
+	badgeIconUrl.value =
+		route.query.iconUrl?.toString() ||
+		"https://badger-staging.worldwidepixel.ca/badger.png";
+});
 
 // SAVE ON EDIT
 
-watch([topText, topTextColor, topBackgroundColor, bottomText, bottomTextColor, bottomBackgroundColor, badgeIconUrl], (newValues) => {
-	const [topText, topTextColor, topBackgroundColor, bottomText, bottomTextColor, bottomBackgroundColor, badgeIconUrl] = newValues
-	if (process.client) {
-		localStorage.topText = topText;
-		localStorage.topTextColor = topTextColor;
-		localStorage.topBackgroundColor = topBackgroundColor;
-		localStorage.bottomText = bottomText;
-		localStorage.bottomTextColor = bottomTextColor;
-		localStorage.bottomBackgroundColor = bottomBackgroundColor;
-		localStorage.badgeIconUrl = badgeIconUrl;
-	}
-})
+watch(
+	[
+		topText,
+		topTextColor,
+		topBackgroundColor,
+		bottomText,
+		bottomTextColor,
+		bottomBackgroundColor,
+		badgeIconUrl,
+	],
+	(newValues) => {
+		const [
+			topText,
+			topTextColor,
+			topBackgroundColor,
+			bottomText,
+			bottomTextColor,
+			bottomBackgroundColor,
+			badgeIconUrl,
+		] = newValues;
+		if (process.client) {
+			localStorage.topText = topText;
+			localStorage.topTextColor = topTextColor;
+			localStorage.topBackgroundColor = topBackgroundColor;
+			localStorage.bottomText = bottomText;
+			localStorage.bottomTextColor = bottomTextColor;
+			localStorage.bottomBackgroundColor = bottomBackgroundColor;
+			localStorage.badgeIconUrl = badgeIconUrl;
+		}
+	},
+);
 
-$resetBus.$on('reset', () => resetToDefault())
+$resetBus.$on("reset", () => resetToDefault());
 
 //watchEffect(async () => badgeIcon.value = await toBase64(badgeIconRaw.value))
 
@@ -73,29 +109,29 @@ async function toBase64(file: File) {
 			reader.readAsDataURL(file);
 			reader.onload = () => resolve(reader.result);
 			reader.onerror = (error) => reject(error);
-		})
-		return await encoded
+		});
+		return await encoded;
 	} catch {
 		//console.log(await urlToData('/badger.png'))
-		return await urlToData('/badger.png')
+		return await urlToData("/badger.png");
 	}
 }
 
 async function resetToDefault() {
-	topText.value = "Made for"
-	topTextColor.value = "#FFFFFF"
-	topBackgroundColor.value = "#8F004C"
-	bottomText.value = "You"
-	bottomTextColor.value = "#FF0066"
-	bottomBackgroundColor.value = "#61003D"
-	badgeIconUrl.value = 'https://badger-staging.worldwidepixel.ca/badger.png'
+	topText.value = "Made for";
+	topTextColor.value = "#FFFFFF";
+	topBackgroundColor.value = "#8F004C";
+	bottomText.value = "You";
+	bottomTextColor.value = "#FF0066";
+	bottomBackgroundColor.value = "#61003D";
+	badgeIconUrl.value = "https://badger-staging.worldwidepixel.ca/badger.png";
 }
 
 function getEmbeddableIcon() {
 	if (route.query.iconUrl != null) {
-		return `https://badger-staging.worldwidepixel.ca/cozy_minimal?gradientStart=${topBackgroundColor.value.replace('#', '')}&gradientEnd=${bottomBackgroundColor.value.replace('#', '')}&iconUrl=${badgeIconUrl.value}&format=png`
+		return `https://badger-staging.worldwidepixel.ca/cozy_minimal?gradientStart=${topBackgroundColor.value.replace("#", "")}&gradientEnd=${bottomBackgroundColor.value.replace("#", "")}&iconUrl=${badgeIconUrl.value}&format=png`;
 	} else {
-		return 'https://badger-staging.worldwidepixel.ca/cozy_minimal?gradientStart=8F004C&gradientEnd=61003D&iconUrl=https://badger-staging.worldwidepixel.ca/badger.png&format=png'
+		return "https://badger-staging.worldwidepixel.ca/cozy_minimal?gradientStart=8F004C&gradientEnd=61003D&iconUrl=https://badger-staging.worldwidepixel.ca/badger.png&format=png";
 	}
 }
 
@@ -104,90 +140,80 @@ useSeoMeta({
 	ogTitle: "Badger: A badge creator for the web",
 	description: "Create beautiful modern, and unique badges using Badger.",
 	ogDescription: "Create beautiful modern, and unique badges using Badger.",
-	ogImage: () => `${getEmbeddableIcon()}`
-})
-
+	ogImage: () => `${getEmbeddableIcon()}`,
+});
 </script>
 
 <template>
-
 	<div class="dark:text-white dark:bg-slate-950 flex flex-row h-full">
-
 		<div class="flex flex-col gap-4 grow shrink-0 p-4">
-
 			<span class="font-bold text-3xl"> Edit </span>
 
 			<div class="w-fit flex flex-col gap-2 p-4 rounded-3xl border">
-
 				<span class="w-fit text-xl font-medium"> Background </span>
 
-				<span class="w-fit flex flex-row gap-4 items-center"> Top
+				<span class="w-fit flex flex-row gap-4 items-center">
+					Top
 					<ColorInput class="h-10" v-model="topBackgroundColor" />
 				</span>
 
-				<span class="w-fit flex flex-row gap-4 items-center"> Bottom
+				<span class="w-fit flex flex-row gap-4 items-center">
+					Bottom
 					<ColorInput class="h-10" v-model="bottomBackgroundColor" />
 				</span>
-
 			</div>
 
 			<div class="w-fit flex flex-col gap-2 p-4 rounded-3xl border">
-
 				<span class="text-xl font-medium"> Icon </span>
 
 				<span class="w-fit flex flex-row gap-4 items-center">
-
 					<TextInput v-model="badgeIconUrl" />
 
 					<!--This could come back... eventually.-->
 					<!--<img class="p-1 w-10 h-10 rounded-xl border" :src="badgeIconUrl">-->
-
 				</span>
-
 			</div>
 
 			<div class="w-fit flex flex-col gap-2 p-4 rounded-3xl border">
-
 				<span class="text-xl font-medium"> Text </span>
 
-				<span class="w-fit flex flex-col gap-2"> Top
+				<span class="w-fit flex flex-col gap-2">
+					Top
 					<TextInput v-model="topText" />
 					<ColorInput class="h-10" v-model="topTextColor" />
 				</span>
 
-				<span class="w-fit flex flex-col gap-2"> Bottom
+				<span class="w-fit flex flex-col gap-2">
+					Bottom
 					<TextInput v-model="bottomText" />
 					<ColorInput class="h-10" v-model="bottomTextColor" />
 				</span>
-
 			</div>
-
 		</div>
 
-		<div class="flex flex-col gap-4 border-l grow-0 shrink flex-shrink-0 p-4">
-
+		<div
+			class="flex flex-col gap-4 border-l grow-0 shrink flex-shrink-0 p-4"
+		>
 			<span class="font-bold text-3xl"> View </span>
 
 			<span class="flex flex-row gap-2">
+				<img
+					:src="`https://badger-staging.worldwidepixel.ca/cozy?gradientStart=${topBackgroundColor.replace('#', '')}&gradientEnd=${bottomBackgroundColor.replace('#', '')}&lineOne=${encodeURI(topText)}&lineTwo=${encodeURI(bottomText)}&colourOne=${topTextColor.replace('#', '')}&colourTwo=${bottomTextColor.replace('#', '')}&iconUrl=${badgeIconUrl}`"
+				/>
 
 				<img
-					:src="`https://badger-staging.worldwidepixel.ca/cozy?gradientStart=${topBackgroundColor.replace('#', '')}&gradientEnd=${bottomBackgroundColor.replace('#', '')}&lineOne=${encodeURI(topText)}&lineTwo=${encodeURI(bottomText)}&colourOne=${topTextColor.replace('#', '')}&colourTwo=${bottomTextColor.replace('#', '')}&iconUrl=${badgeIconUrl}`">
-
-				<img
-					:src="`https://badger-staging.worldwidepixel.ca/cozy_minimal?gradientStart=${topBackgroundColor.replace('#', '')}&gradientEnd=${bottomBackgroundColor.replace('#', '')}&iconUrl=${badgeIconUrl}`">
-
+					:src="`https://badger-staging.worldwidepixel.ca/cozy_minimal?gradientStart=${topBackgroundColor.replace('#', '')}&gradientEnd=${bottomBackgroundColor.replace('#', '')}&iconUrl=${badgeIconUrl}`"
+				/>
 			</span>
 			<span class="flex flex-row gap-2">
+				<img
+					:src="`https://badger-staging.worldwidepixel.ca/compact?gradientStart=${topBackgroundColor.replace('#', '')}&gradientEnd=${bottomBackgroundColor.replace('#', '')}&lineOne=${encodeURI(topText)}&lineTwo=${encodeURI(bottomText)}&colourOne=${topTextColor.replace('#', '')}&colourTwo=${bottomTextColor.replace('#', '')}&iconUrl=${badgeIconUrl}`"
+				/>
 
 				<img
-					:src="`https://badger-staging.worldwidepixel.ca/compact?gradientStart=${topBackgroundColor.replace('#', '')}&gradientEnd=${bottomBackgroundColor.replace('#', '')}&lineOne=${encodeURI(topText)}&lineTwo=${encodeURI(bottomText)}&colourOne=${topTextColor.replace('#', '')}&colourTwo=${bottomTextColor.replace('#', '')}&iconUrl=${badgeIconUrl}`">
-
-				<img
-					:src="`https://badger-staging.worldwidepixel.ca/cozy_minimal?gradientStart=${topBackgroundColor.replace('#', '')}&gradientEnd=${bottomBackgroundColor.replace('#', '')}&iconUrl=${badgeIconUrl}`">
-
+					:src="`https://badger-staging.worldwidepixel.ca/cozy_minimal?gradientStart=${topBackgroundColor.replace('#', '')}&gradientEnd=${bottomBackgroundColor.replace('#', '')}&iconUrl=${badgeIconUrl}`"
+				/>
 			</span>
 		</div>
-
 	</div>
-
 </template>
