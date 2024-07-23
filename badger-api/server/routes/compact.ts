c7d2882044014651acc338c6ce1e140601badf1f
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-07-22 17:52:28 -0700

    Add Full-Strokeless to v4

--- badger-api/server/routes/compact.ts
+++ badger-api/server/routes/compact.ts
@@ -1,82 +1,86 @@
 import { Font } from "opentype.js";
-import sharp from 'sharp';
+import sharp from "sharp";
 import { getInterExtraBold, getInterMedium } from "../lib/fonts";
 
 export default defineEventHandler(async (event) => {
-	const interMedium = await getInterMedium();
-	const interExtraBold = await getInterExtraBold();
+  const interMedium = await getInterMedium();
+  const interExtraBold = await getInterExtraBold();
 
-	try {
-		const query = getQuery(event);
-		const gradientStart = query.gradientStart;
-		const gradientEnd = query.gradientEnd;
-		const lineOne = query.lineOne ?? "undefined";
-		const lineTwo = query.lineTwo ?? "undefined";
-		const colorOne = query.colorOne ?? query.colourOne;
-		const colorTwo = query.colorTwo ?? query.colourTwo;
-		const iconUrl = query.iconUrl;
-		const format = query.format ?? 'svg';
+  try {
+    const query = getQuery(event);
+    const gradientStart = query.gradientStart;
+    const gradientEnd = query.gradientEnd;
+    const lineOne = query.lineOne ?? "undefined";
+    const lineTwo = query.lineTwo ?? "undefined";
+    const colorOne = query.colorOne ?? query.colourOne;
+    const colorTwo = query.colorTwo ?? query.colourTwo;
+    const iconUrl = query.iconUrl;
+    const format = query.format ?? "svg";
 
-		function getWidth(text: any, size: any, font: Font) {
-			const fontSize = size;
-			const fontScale = (1 / font.unitsPerEm) * fontSize;
+    function getWidth(text: any, size: any, font: Font) {
+      const fontSize = size;
+      const fontScale = (1 / font.unitsPerEm) * fontSize;
 
-			let width = 0;
-			const glyphs = font.stringToGlyphs(text);
-			for (let i = 0; i < glyphs.length; i++) {
-				const glyph = glyphs[i];
+      let width = 0;
+      const glyphs = font.stringToGlyphs(text);
+      for (let i = 0; i < glyphs.length; i++) {
+        const glyph = glyphs[i];
 
-				if (glyph.advanceWidth) {
-					width += glyph.advanceWidth * fontScale;
-				}
-			}
-			return width;
-		}
+        if (glyph.advanceWidth) {
+          width += glyph.advanceWidth * fontScale;
+        }
+      }
+      return width;
+    }
 
-		const mediumPathData = interMedium.getPath(
-			lineOne.toString(),
-			42,
-			26.5,
-			17,
-		);
-		mediumPathData.fill = `#${colorOne}`;
-		const mediumPath = mediumPathData.toSVG(2);
-		const mediumWidth = getWidth(lineOne.toString(), 17, interMedium);
+    const mediumPathData = interMedium.getPath(
+      lineOne.toString(),
+      42,
+      26.5,
+      17
+    );
+    mediumPathData.fill = `#${colorOne}`;
+    const mediumPath = mediumPathData.toSVG(2);
+    const mediumWidth = getWidth(lineOne.toString(), 17, interMedium);
 
-		const extraBoldPathData = interExtraBold.getPath(
-			lineTwo.toString(),
-			mediumWidth + 42 + 3.6,
-			26.5,
-			17,
-		);
-		extraBoldPathData.fill = `#${colorTwo}`;
-		const extraBoldPath = extraBoldPathData.toSVG(2);
-		const extraBoldWidth = getWidth(lineTwo.toString(), 17, interExtraBold);
+    const extraBoldPathData = interExtraBold.getPath(
+      lineTwo.toString(),
+      mediumWidth + 42 + 3.6,
+      26.5,
+      17
+    );
+    extraBoldPathData.fill = `#${colorTwo}`;
+    const extraBoldPath = extraBoldPathData.toSVG(2);
+    const extraBoldWidth = getWidth(lineTwo.toString(), 17, interExtraBold);
 
-		const OTFinalWidth =
-			mediumWidth > extraBoldWidth ? mediumWidth : extraBoldWidth;
+    const OTFinalWidth =
+      mediumWidth > extraBoldWidth ? mediumWidth : extraBoldWidth;
 
-		const width = mediumWidth + 3.6 + extraBoldWidth + 42 + 8.5;
+    const width = mediumWidth + 3.6 + extraBoldWidth + 42 + 8.5;
 
-		async function toBase64ImageUrl(imgUrl: string): Promise<string> {
-			const fetchImageUrl = await fetch(imgUrl);
-			const responseArrBuffer = await fetchImageUrl.arrayBuffer();
-			const toBase64 = `data:${fetchImageUrl.headers.get("Content-Type") || "image/png"};base64,${Buffer.from(responseArrBuffer).toString("base64")}`;
-			return toBase64;
-		}
+    async function toBase64ImageUrl(imgUrl: string): Promise<string> {
+      const fetchImageUrl = await fetch(imgUrl);
+      const responseArrBuffer = await fetchImageUrl.arrayBuffer();
+      const toBase64 = `data:${
+        fetchImageUrl.headers.get("Content-Type") || "image/png"
+      };base64,${Buffer.from(responseArrBuffer).toString("base64")}`;
+      return toBase64;
+    }
 
-		let imageUrl = iconUrl;
+    let imageUrl = iconUrl;
 
-		if (imageUrl?.toString().includes("http" || "https")) {
-			imageUrl = await toBase64ImageUrl(imageUrl.toString());
-		}
+    if (imageUrl?.toString().includes("http" || "https")) {
+      imageUrl = await toBase64ImageUrl(imageUrl.toString());
+    }
 
-		console.log(width)
+    //console.log(width)
 
-		let finalSvg = `
+    let finalSvg = `
 		<svg width="${width}" height="40" viewBox="0 0 ${width} 40" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 			<rect width="${width}" height="40" rx="8" fill="url(#paint0_linear_1_56)"/>
-			<rect x="1.07143" y="1.07143" width="${width - 2}" height="37.8585" rx="6.92857" stroke="white" stroke-opacity="0.15" stroke-width="2.14286"/>
+			<rect x="1.07143" y="1.07143" width="${
+        width - 2
+      }" height="37.8585" rx="6.92857" stroke="white" stroke-opacity="0.15" stroke-width="2.14286"/>
 			<g filter="url(#filter0_d_1_56)">
 				<rect x="8" y="6" width="28" height="28" fill="url(#pattern0_1_56)"/>
 			</g>
@@ -114,19 +118,18 @@ export default defineEventHandler(async (event) => {
 			</linearGradient>
       		<image id="image0_1_56" width="512" height="512" xlink:href="${imageUrl}" />
 		</defs>
-	</svg>`
+	</svg>`;
 
-		if (format === 'png') {
-			setHeader(event, "Content-Type", "image/png");
-			let svgBuffer = Buffer.from(finalSvg, 'utf-8')
-			let convertedPng = sharp(svgBuffer).png().toBuffer()
-			return convertedPng;
-		} else {
-			setHeader(event, "Content-Type", "image/svg+xml");
-			return finalSvg;
-		}
-
-	} catch {
-		return Error;
-	}
+    if (format === "png") {
+      setHeader(event, "Content-Type", "image/png");
+      let svgBuffer = Buffer.from(finalSvg, "utf-8");
+      let convertedPng = sharp(svgBuffer).png().toBuffer();
+      return convertedPng;
+    } else {
+      setHeader(event, "Content-Type", "image/svg+xml");
+      return finalSvg;
+    }
+  } catch {
+    return Error;
+  }
 });
--- badger-api/server/routes/v4/full_strokeless.ts
+++ badger-api/server/routes/v4/full_strokeless.ts
@@ -72,46 +72,34 @@ export default defineEventHandler(async (event) => {
 
     let finalSvg = `
 <svg width="${width}" height="44" viewBox="0 0 ${width} 44" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-	<rect x="1" y="1" width="${
-    width - 2
-  }" height="42" rx="7" fill="#${backgroundColour}"/>
-	<rect x="1" y="1" width="${
-    width - 2
-  }" height="42" rx="7" fill="black" fill-opacity="0.7"/>
-	<rect x="1" y="1" width="${
-    width - 2
-  }" height="42" rx="7" fill="url(#paint0_linear_1_99)" fill-opacity="0.3" style="mix-blend-mode:overlay"/>
-	<rect x="1" y="1" width="${
-    width - 2
-  }" height="42" rx="7" stroke="#${backgroundColour}" stroke-width="2"/>
-	<rect x="1" y="1" width="${
-    width - 2
-  }" height="42" rx="7" stroke="black" stroke-opacity="0.3" stroke-width="2"/>
-	<g filter="url(#filter0_d_1_99)">
-	<rect x="6" y="6" width="32" height="32" fill="url(#pattern0_1_99)"/>
-	</g>
-				${mediumPath}
-				${extraBoldPath}
-	<defs>
-		<filter id="filter0_d_1_99" x="0.285715" y="0.285715" width="43.4286" height="43.4286" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-			<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-			<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-			<feOffset/>
-			<feGaussianBlur stdDeviation="2.85714"/>
-			<feComposite in2="hardAlpha" operator="out"/>
-			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_99"/>
-			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_99" result="shape"/>
-		</filter>
-		<pattern id="pattern0_1_99" patternContentUnits="objectBoundingBox" width="1" height="1">
-			<use xlink:href="#image0_1_99" transform="scale(0.00195312)"/>
-		</pattern>
-		<linearGradient id="paint0_linear_1_99" x1="65.5" y1="0" x2="65.5" y2="44" gradientUnits="userSpaceOnUse">
-			<stop stop-color="white"/>
-			<stop offset="1" stop-color="white" stop-opacity="0"/>
-		</linearGradient>
-		<image id="image0_1_99" width="512" height="512" xlink:href="${imageUrl}" />
-	</defs>
+  <rect width="${width}" height="44" rx="8" fill="#${backgroundColour}"/>
+  <rect width="${width}" height="44" rx="8" fill="black" fill-opacity="0.7"/>
+  <rect width="${width}" height="44" rx="8" fill="url(#paint0_linear_19_2)" fill-opacity="0.3" style="mix-blend-mode:overlay"/>
+  <g filter="url(#filter0_d_19_2)">
+    <rect x="6" y="6" width="32" height="32" fill="url(#pattern0_19_2)"/>
+  </g>
+  ${mediumPath}
+  ${extraBoldPath} 
+  <defs>
+    <filter id="filter0_d_19_2" x="0.285715" y="0.285715" width="43.4286" height="43.4286" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset/>
+      <feGaussianBlur stdDeviation="2.85714"/>
+      <feComposite in2="hardAlpha" operator="out"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_19_2"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_19_2" result="shape"/>
+    </filter>
+    <pattern id="pattern0_19_2" patternContentUnits="objectBoundingBox" width="1" height="1">
+      <use xlink:href="#image0_19_2" transform="scale(0.00195312)"/>
+    </pattern>
+    <linearGradient id="paint0_linear_19_2" x1="65.5" y1="0" x2="65.5" y2="44" gradientUnits="userSpaceOnUse">
+      <stop stop-color="white"/>
+      <stop offset="1" stop-color="white" stop-opacity="0"/>
+    </linearGradient>
+    <image id="image0_19_2" width="512" height="512" xlink:href="${imageUrl}" />
+  </defs>
 </svg>
 `;
 
