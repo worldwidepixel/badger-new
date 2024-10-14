80d61a5a0c6b7b1705dcaa66b0927b56485733a9
worldwidepixel <58098422+worldwidepixel@users.noreply.github.com>
2024-10-13 18:18:52 -0700

    OG 2...!

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,19 +1,3 @@
 <template>
 	<NuxtLayout />
 </template>
-
-<script setup>
-const route = useRoute();
-useHead({
-	title: "Badger",
-	titleTemplate: "%s - Badger",
-	meta: [
-		{
-			name: "description",
-			content: "Create beautiful, modern, and unique badges using Badger.",
-		},
-		{ name: "theme-color", content: "#ff3a40" },
-	],
-	link: [{ rel: "shortcut icon", href: "./badge.png", type: "image/png" }],
-});
-</script>
