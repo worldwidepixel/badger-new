495f51be126c87b38c733eb7f37c5cc55fd44b6e
worldwidepixel <worldwidepxl@gmail.com>
2025-07-11 01:25:20 -0700

    feat: start badger v3

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="flex flex-col">
+	<nav class="flex w-auto justify-center border-b p-5">
+		<div
+			class="flex w-full max-w-[80rem] flex-row items-center justify-evenly sm:justify-between"
+		>
+			badger 3: echoes of the prequel
+		</div>
+	</nav>
+	{@render children()}
+</div>
