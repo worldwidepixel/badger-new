e03c30a9fc8d6b481d7176e745c5049091bdf891
worldwidepixel <worldwidepxl@gmail.com>
2025-07-12 21:26:46 -0700

    fix: Desparate attempt #2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	build: {
-		rollupOptions: {
-			external: ['xss', 'opentype.js']
-		}
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
