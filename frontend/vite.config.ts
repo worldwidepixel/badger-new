03b2500db0e13c12fc12f3a88b0c1765c39d2c6c
worldwidepixel <worldwidepxl@gmail.com>
2025-07-12 22:38:45 -0700

    fix: It built, so

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: { xss: ['xss'], 'opentype.js': ['opentype.js'] }
-			}
-		}
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
