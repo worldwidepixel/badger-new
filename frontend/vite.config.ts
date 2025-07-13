22093214c255e9feda8e46ace0a50207f1dfdae8
worldwidepixel <worldwidepxl@gmail.com>
2025-07-12 21:06:51 -0700

    tweak: Rollupoptions for XSS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		rollupOptions: {
+			external: ['xss']
+		}
+	}
 });
