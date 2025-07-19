ad8cea11336411e185bf7f57e600dffc1e1448c1
worldwidepixel <worldwidepxl@gmail.com>
2025-07-19 00:22:12 -0700

    fix: Lie to Node; do crime

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	ssr: {
+		noExternal: ['opentype.js']
+	},
+	resolve: {
+		alias: {
+			fs: path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src/lib/fake-fs.ts')
+		}
+	}
 });
