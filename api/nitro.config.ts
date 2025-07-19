438956676a8b20f29c598ef80fd22cd04706f22e
worldwidepixel <worldwidepxl@gmail.com>
2025-07-19 16:38:58 -0700

    tweak(api): 6

--- api/nitro.config.ts
+++ api/nitro.config.ts
@@ -3,10 +3,4 @@ export default defineNitroConfig({
 	srcDir: "src",
 	compatibilityDate: "2025-07-19",
 	errorHandler: "~/error.ts",
-	noExternals: true,
-	node: false,
-
-	externals: {
-		external: ["sharp", "@img/sharp-wasm32"],
-	},
 });
