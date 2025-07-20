dda1640e8113670cdeed9828e967ef3dc86e652a
worldwidepixel <worldwidepxl@gmail.com>
2025-07-19 17:15:57 -0700

    tweak(api): 8

--- api/nitro.config.ts
+++ api/nitro.config.ts
@@ -3,12 +3,5 @@ export default defineNitroConfig({
 	srcDir: "src",
 	compatibilityDate: "2025-07-19",
 	errorHandler: "~/error.ts",
-	noExternals: true,
 	node: false,
-
-	rollupConfig: {
-		external(id) {
-			id.includes("sharp");
-		},
-	},
 });
