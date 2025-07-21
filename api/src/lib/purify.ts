5a30cb78dd6bf6f72b469dce4acbfc98bb71c778
worldwidepixel <worldwidepxl@gmail.com>
2025-07-21 00:49:45 -0700

    feat(api): Remove support for old people

--- api/src/lib/purify.ts
+++
@@ -1,7 +0,0 @@
-import { santitiseText } from "@badgered/common";
-
-/** @deprecated */
-export async function purifyOutput(input: string): Promise<String> {
-	const cleanOutput = santitiseText(input);
-	return cleanOutput;
-}
