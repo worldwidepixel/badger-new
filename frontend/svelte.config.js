fc46476f4f11fc89292a18352f5f9888c14c69ba
worldwidepixel <worldwidepxl@gmail.com>
2025-07-27 23:20:11 -0700

    fix(frontend): It's a public API who cares

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		csrf: {
+			checkOrigin: false
+		}
+	}
 };
 
 export default config;
