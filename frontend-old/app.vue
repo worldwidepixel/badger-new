96091e1ba0576784d5cbf14e6f501df3bfb019ae
worldwidepixel <worldwidepxl@gmail.com>
2026-02-14 20:45:48 -0800

    remove frontend-old reference

--- frontend-old/app.vue
+++
@@ -1,3 +0,0 @@
-<template>
-	<NuxtLayout />
-</template>
--- frontend-old/plugins/tooltips.client.ts
+++
@@ -1,9 +0,0 @@
-import FloatingVue from "floating-vue";
-
-export default defineNuxtPlugin(() => {
-	FloatingVue.options.themes["badger-ui"] = {
-		...FloatingVue.options.themes.tooltip,
-		$resetCss: true,
-		placement: "bottom",
-	};
-});
