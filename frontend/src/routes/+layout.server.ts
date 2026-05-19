16d024613983afc6945e79e5bdfd971f5d4aa432
worldwidepixel <worldwidepxl@gmail.com>
2026-05-18 17:55:06 -0700

    fix: workers test

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,10 +1,14 @@
-import { CF_PAGES_BRANCH, CF_PAGES_COMMIT_SHA } from '$env/static/private';
+//import { CF_PAGES_BRANCH, CF_PAGES_COMMIT_SHA } from '$env/static/private';
+import {
+	WORKERS_CI_BRANCH as BRANCH,
+	WORKERS_CI_COMMIT_SHA as COMMIT_SHA
+} from '$env/static/private';
 
 export function load({ cookies }) {
 	return {
 		deployment_info: {
-			branch: CF_PAGES_BRANCH ?? 'local',
-			git_hash: CF_PAGES_COMMIT_SHA ?? 'unknown'
+			branch: BRANCH ?? 'local',
+			git_hash: COMMIT_SHA ?? 'unknown'
 		},
 		savedThemeProfile: cookies.get('themeProfile')
 	};
